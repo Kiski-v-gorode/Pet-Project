7b6824221abf1f784c7a630c89298102aeb1719e
muhammad <kurbanovmagomed444@gmail.com>
2023-12-11 21:07:42 +0300

    Реализовал алгоритм сортировки, а мб и не реализовал

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 import json
 
-json_file_path = r"/home/muhammad/Desktop/Pet-Project/soccer.json"
+json_file_path = r"D:\Рабочий стол\Pet Project\soccer.json"
 
 with open(json_file_path, "r", encoding="utf-8") as file:
     json_content = json.load(file)
 
 teams = json_content["teams"]
 matches = json_content["matches"]
-
-"""
-4 {'name': 'Локомотив М', 'games': 17, 'won': 7, 'drawn': 7, 'lost': 3, 'goals_scored': 27, 'missed_scored': 23, 'goal_difference': 4, 'points': 28, 'head_to_head': {'Рубин': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'Факел': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 4}, 'ЦСКА': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 4, 'missed_scored': 1}, 'Крылья Советов': {'won': 0, 'drawn': 2, 'lost': 0, 'goals_scored': 4, 'missed_scored': 4}, 'Краснодар': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Сочи': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 1}, 'Балтика': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 2, 'missed_scored': 3}, 'Оренбург': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 0}, 'Зенит': {'won': 0, 'drawn': 0, 'lost': 2, 'goals_scored': 2, 'missed_scored': 5}, 'Пари Нижний Новгород': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 1}, 'Урал': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'Динамо М': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 0, 'missed_scored': 0}, 'Ростов': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 1, 'missed_scored': 0}, 'Спартак М': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Ахмат': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 2}}}
-5 {'name': 'ЦСКА', 'games': 17, 'won': 7, 'drawn': 7, 'lost': 3, 'goals_scored': 31, 'missed_scored': 23, 'goal_difference': 8, 'points': 28, 'head_to_head': {'Урал': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 1}, 'Ахмат': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 2, 'missed_scored': 3}, 'Локомотив М': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 4}, 'Сочи': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 3}, 'Динамо М': {'won': 2, 'drawn': 0, 'lost': 0, 'goals_scored': 5, 'missed_scored': 3}, 'Оренбург': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Зенит': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Крылья Советов': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'Ростов': {'won': 0, 'drawn': 1, 'lost': 1, 'goals_scored': 3, 'missed_scored': 5}, 'Балтика': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 1}, 'Спартак М': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'Рубин': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 0, 'missed_scored': 0}, 'Краснодар': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 1}, 'Пари Нижний Новгород': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 2, 'missed_scored': 3}, 'Факел': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}}}
-9 {'name': 'Ростов', 'games': 17, 'won': 5, 'drawn': 5, 'lost': 7, 'goals_scored': 24, 'missed_scored': 29, 'goal_difference': -5, 'points': 20, 'head_to_head': {'Факел': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 2}, 'Зенит': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Крылья Советов': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 5, 'missed_scored': 1}, 'Рубин': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 3}, 'Сочи': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 4, 'missed_scored': 0}, 'Пари Нижний Новгород': {'won': 1, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 1}, 'Динамо М': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 1}, 'Балтика': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'ЦСКА': {'won': 1, 'drawn': 1, 'lost': 0, 'goals_scored': 5, 'missed_scored': 3}, 'Урал': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 2}, 'Краснодар': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 3, 'missed_scored': 2}, 'Ахмат': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 3}, 'Локомотив М': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 1}, 'Оренбург': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}, 'Спартак М': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 1}}}
-10 {'name': 'Факел', 'games': 17, 'won': 5, 'drawn': 5, 'lost': 7, 'goals_scored': 14, 'missed_scored': 18, 'goal_difference': -4, 'points': 20, 'head_to_head': {'Ростов': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 1}, 'Локомотив М': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 4, 'missed_scored': 1}, 'Балтика': {'won': 1, 'drawn': 1, 'lost': 0, 'goals_scored': 2, 'missed_scored': 1}, 'Зенит': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 0}, 'Пари Нижний Новгород': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 2}, 'Краснодар': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 0, 'missed_scored': 0}, 'Крылья Советов': {'won': 1, 'drawn': 0, 'lost': 0, 'goals_scored': 3, 'missed_scored': 0}, 'Урал': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 0, 'missed_scored': 0}, 'Рубин': {'won': 2, 'drawn': 0, 'lost': 0, 'goals_scored': 2, 'missed_scored': 0}, 'Сочи': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 2}, 'Динамо М': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 0, 'missed_scored': 0}, 'Оренбург': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 2}, 'Спартак М': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 0, 'missed_scored': 2}, 'Ахмат': {'won': 0, 'drawn': 0, 'lost': 1, 'goals_scored': 1, 'missed_scored': 2}, 'ЦСКА': {'won': 0, 'drawn': 1, 'lost': 0, 'goals_scored': 1, 'missed_scored': 1}}}
-"""
-
-
-# team1 - это JSON первой команды
-# team2 - это JSON это второй команды
-# возвращаем два элемента, первый, команда которая должна быть раньше в таблице и вторая команда
-def stagings_sorting(team1, team2):
-    team1_name, team2_name = team1["name"], team2["name"]
-
-    criteria = [
-        lambda t, o: t["head_to_head"][o]["won"] + t["head_to_head"][o]["drawn"],
-        lambda t, o: t["head_to_head"][o]["won"],
-        lambda t, o: t["head_to_head"][o]["goals_scored"] - t["head_to_head"][o]["missed_scored"],
-        lambda t, o: t["head_to_head"][o]["goals_scored"]
-    ]
-
-    for criterion in criteria:
-        points1, points2 = criterion(team1, team2_name), criterion(team2, team1_name)
-        if points1 > points2:
-            return team1, team2
-        elif points1 < points2:
-            return team2, team1
-
-    return team1, team2
 
 
 def generate_standings(teams, matches):
@@ -121,37 +91,46 @@ def generate_standings(teams, matches):
                 standings[guest]["head_to_head"][standings[owner]["name"]]["lost"] += 1
                 standings[owner]["head_to_head"][standings[guest]["name"]]["won"] += 1
 
-        # else:
-        #     standings = sorted(standings.values(), key=lambda team: team["points"], reverse=True)
-    # return standings
-    sub_standings = sorted(standings.values(), key=lambda team: team["points"], reverse=True)
-    # print(sub_standings)
-    standings = []
-    team = 0
-    teams = len(sub_standings) - 1
+    standings = sorted(standings.values(), key=lambda team: (team["points"], team["won"], team["goal_difference"], team["goals_scored"]), reverse=True)
 
-    while team <= teams:
-        if team != teams and sub_standings[team]["points"] == sub_standings[team + 1]["points"]:
-            standings.extend(stagings_sorting(sub_standings[team], sub_standings[team + 1]))
-            # Чтобы не вставлять обработанное значение дважды
-            team += 1
+    for i in range(len(standings) - 1):
+        current_team = standings[i]
+        next_team = standings[i + 1]
 
-        else:
-            standings.append(sub_standings[team])
+        if current_team["points"] == next_team["points"]:
 
-        team += 1
-    # for team in range(len(sub_standings) - 1):
-    #     if sub_standings[team]["points"] == sub_standings[team + 1]["points"]:
-    #         print(team)
-    #     else:
-    #         print(team)
-    # print(f"{team, sub_standings[team]}")
-    # print(f"{team + 1, sub_standings[team + 1]}")
+            team1_name, team2_name = current_team["name"], next_team["name"]
 
-    # print(standings)
-    # standings = sorted(standings.values(), key=lambda team: (team["points"], team["won"], team["goal_difference"], team["goals_scored"]), reverse=True)
-    #
+            # Сколько раз team2 выйграла у team1 + сколь раз они сыграли в ничью и наоборот (Считаю количество очков команд)
+            points2 = (current_team["head_to_head"][team2_name]["won"] * 3) + current_team["head_to_head"][team2_name]["drawn"]
+            points1 = (next_team["head_to_head"][team1_name]["won"] * 3) + next_team["head_to_head"][team1_name]["drawn"]
+
+            # Сколько раз team2 выйграла у team1 и наоборот
+            won2 = current_team["head_to_head"][team2_name]["won"]
+            won1 = next_team["head_to_head"][team1_name]["won"]
+
+            # Сколько раз team2 забила и пропустила от team1 и наоборот (Считаю разницу голов в личных встречах)
+            goal_difference2 = current_team["head_to_head"][team2_name]["goals_scored"] - current_team["head_to_head"][team2_name]["missed_scored"]
+            goal_difference1 = next_team["head_to_head"][team1_name]["goals_scored"] - next_team["head_to_head"][team1_name]["missed_scored"]
+
+            # Сколько раз team2 забила team1 и наоборот
+            goals2 = current_team["head_to_head"][team2_name]["goals_scored"]
+            goals1 = next_team["head_to_head"][team1_name]["goals_scored"]
+
+            if points2 > points1:
+                standings[i], standings[i + 1] = next_team, current_team
+
+            elif won2 > won1:
+                standings[i], standings[i + 1] = next_team, current_team
+
+            elif goal_difference2 > goal_difference1:
+                standings[i], standings[i + 1] = next_team, current_team
+
+            elif goals2 > goals1:
+                standings[i], standings[i + 1] = next_team, current_team
+
     print(f'{"Место":<5}{"Клуб":>20}{"И":>5}{"В":>5}{"Н":>5}{"П":>5}{"ЗМ":>5}{"ПМ":>5}{"РМ":>5}{"О":>5}')
+
     for index in range(len(standings)):
 
         print(f'{index + 1:<5}{standings[index]["name"]:>20}{standings[index]["games"]:>5} {standings[index]["won"]:>5}'
@@ -159,7 +138,7 @@ def generate_standings(teams, matches):
               f'{standings[index]["missed_scored"]:>5}{standings[index]["goal_difference"]:>5}{standings[index]["points"]:>5}')
 
 
-print(generate_standings(teams=teams, matches=matches))
+generate_standings(teams=teams, matches=matches)
 
 """
 Нужно хранить исход личных встреч (набранные очки, количество побед, разницу голов, число забитых мячей) для каждой команды 
