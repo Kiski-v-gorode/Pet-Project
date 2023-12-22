7b4314d09748b5d5261e5de03f37998b36ac9e6a
muhammad <kurbanovmagomed444@gmail.com>
2023-12-22 17:08:20 +0300

    Игрался с rabbitmq

--- rabbitmq_producer.py
+++ rabbitmq_producer.py
@@ -1,4 +1,10 @@
-from confluent_kafka import Producer
-import socket
+import pika
 
-conf = {"boot": ""}
+
+connection_params = pika.ConnectionParameters(host='localhost', port=5672)
+connection = pika.BlockingConnection(connection_params)
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
