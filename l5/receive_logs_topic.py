62fc4fdcd93d6a9dd1ad2f831b86ca1925a4c08c
muhammad <kurbanovmagomed444@gmail.com>
2023-12-26 04:41:12 +0300

    Игрался с rabbitmq

---
+++ l5/receive_logs_topic.py
@@ -0,0 +1,42 @@
+import pika, sys, os, time, datetime
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
+
+    binding_keys = sys.argv[1:]
+
+    if not binding_keys:
+        sys.stderr.write(f"Usage: %s [{sys.argv[0]}]...")
+        sys.exit(1)
+
+    for binding_key in binding_keys:
+        channel.queue_bind(queue=queue_name, exchange='topic_logs', routing_key=binding_key)
+
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] {method.routing_key} : {body} -- {datetime.datetime.now()}")
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        print(' Interrupted')
+        try:
+            sys.exit(0)
+
+        except SystemExit:
+            os._exit(0)
